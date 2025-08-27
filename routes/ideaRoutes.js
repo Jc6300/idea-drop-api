f4a89c2314e796c60d51329c5f8f2d3f79b7c858
Josh <joshcotton93@gmail.com>
2025-08-27 16:29:42 -0500

    able to create new idea

--- routes/ideaRoutes.js
+++ routes/ideaRoutes.js
@@ -1,39 +1,77 @@
 import express from "express";
+import Idea from "../models/Idea.js";
+import mongoose from "mongoose";
 
 const router = express.Router();
+
 // @route           GET /api/ideas
 // @description     Get all ideas
 // @access          Public
+router.get("/", async (req, res, next) => {
+  try {
+    const ideas = await Idea.find();
+    res.json(ideas);
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+});
 
-router.get("/", (req, res) => {
-  const ideas = [
-    {
-      id: 1,
-      title: "Idea 1",
-      description: "This is Idea 1",
-    },
-    {
-      id: 2,
-      title: "Idea 2",
-      description: "This is Idea 2",
-    },
-    {
-      id: 3,
-      title: "Idea 3",
-      description: "This is Idea 3",
-    },
-  ];
-  res.json(ideas);
+// @route           GET /api/ideas/:id
+// @description     Get single idea
+// @access          Public
+router.get("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      res.status(404);
+      throw new Error("Idea Not Found");
+    }
+
+    const idea = await Idea.findById(req.params.id);
+    if (!idea) {
+      res.status(404);
+      throw new Error("Idea Not Found");
+    }
+    res.json(idea);
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
 });
 
 // @route           POST/api/ideas
 // @description     Create new Idea
 // @access          Public
-router.post("/", (req, res) => {
-  const { title, description } = req.body;
-  console.log(description);
+router.post("/", async (req, res, next) => {
+  try {
+    const { title, summary, description, tags } = req.body;
 
-  res.send(title);
+    if (!title?.trim() || !summary?.trim() || !description?.trim()) {
+      res.status(400);
+      throw new Error("Title,summary and description are required");
+    }
+    const newIdea = new Idea({
+      title,
+      summary,
+      description,
+      tags:
+        typeof tags === "string"
+          ? tags
+              .split(",")
+              .map((tag) => tag.trim())
+              .filter(Boolean)
+          : Array.isArray(tags)
+          ? tags
+          : [],
+    });
+
+    const savedIdea = await newIdea.save();
+    res.status(201).json(savedIdea);
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
 });
 
 export default router;
